36caa194ab44e2afde79b22f496b4a86452ca9b5
iacopoff <iacopo.ff@gmail.com>
2024-08-06 15:54:58 +0200

    param learning trainer

--- hython/trainer.py
+++ hython/trainer.py
@@ -253,6 +253,77 @@ class RNNTrainer(AbstractTrainer):
         return arr[:, steps]
 
 
+class ParameterLearningTrainer(AbstractTrainer):
+
+    def __init__(self, params: RNNTrainParams):
+
+        self.P = params  # RNNTrainParams(**params)
+        print(self.P)
+        super(ParameterLearningTrainer, self).__init__(self.P.experiment)
+
+
+    def epoch_step(self, model, dataloader, device, opt=None):
+        running_batch_loss = 0
+        data_points = 0
+
+        epoch_preds = None
+        epoch_targets = None
+        valid_masks = None
+
+        # N T C H W
+        for predictor_b, forcing_b, target_b in dataloader: # predictors, forcings , observations
+          
+            target_b = target_b.to(device)
+            forcing_b = forcing_b.to(device)
+
+            predictor_b = predictor_b.to(device)
+            
+            # Predict wflow parameters
+            parameter = model["transfer_nn"](predictor_b).to(device) #  N C H W -> N C h w   #   N L H W Cout
+
+            X = torch.concat([
+                            parameter.unsqueeze(1).repeat(1, forcing_b.size(1), 1,1,1),
+                            forcing_b], dim=2)
+
+            output = model["surrogate"](X)[0]
+   
+            output = output.flatten(2)
+            target = self.predict_step(target_b).flatten(2)
+
+            valid_mask = target != 0
+
+            batch_sequence_loss = loss_batch(self.P.loss_func, output, target, opt, valid_mask)
+            
+            if epoch_preds is None:
+                epoch_preds = output.detach().cpu().numpy()
+                epoch_targets = target.detach().cpu().numpy()
+                valid_masks = valid_mask.detach().cpu().numpy()
+            else:
+                epoch_preds = np.concatenate(
+                    (epoch_preds, output.detach().cpu().numpy()), axis=0
+                )
+                epoch_targets = np.concatenate(
+                    (epoch_targets, target.detach().cpu().numpy()), axis=0
+                )
+                valid_masks = np.concatenate( (
+                    valid_masks, valid_mask.detach().cpu().numpy()), axis=0)
+
+            
+
+            running_batch_loss += batch_sequence_loss
+
+        epoch_loss = running_batch_loss / len(dataloader)
+
+        metric = metric_epoch(
+            self.P.metric_func, epoch_targets, epoch_preds, self.P.target_names, valid_masks
+        )
+
+        return epoch_loss, metric
+
+    def predict_step(self, arr):
+        """Return the n steps that should be predicted"""
+        
+        return arr[:, -1] # N Ch H W  
 class BasinLumpedTrainer(RNNTrainer):
     def __init__(self, params):
         super(BasinLumpedTrainer, self).__init__(params)
