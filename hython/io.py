0296f569796572c4a3e725d9d4743431e92add03
iacopoff <iacopo.ff@gmail.com>
2024-10-02 10:23:29 +0200

    read zarr

--- hython/io.py
+++ hython/io.py
@@ -73,9 +73,9 @@ def write_to_zarr(
             )
 
 
-def read_from_zarr(url, group=None, multi_index=None, engine="xarray"):
+def read_from_zarr(url, group=None, multi_index=None, engine="xarray", chunks=None):
     if engine == "xarray":
-        ds = xr.open_dataset(url, group=group, engine="zarr")
+        ds = xr.open_dataset(url, group=group, engine="zarr",chunks=chunks)
         if multi_index:
             ds = cfxr.decode_compress_to_multi_index(ds, multi_index)
         return ds
