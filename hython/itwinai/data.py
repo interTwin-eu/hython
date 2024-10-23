a34caaa59d97d38353176217356da2bd68195c98
Iacopo <38247963+iacopoff@users.noreply.github.com>
2024-10-23 14:23:03 +0200

    Update data.py

--- hython/itwinai/data.py
+++ hython/itwinai/data.py
@@ -3,9 +3,8 @@ import xarray as xr
 from itwinai.components import DataProcessor, DataSplitter, monitor_exec
 from hython.io import read_from_zarr
 from hython.sampler import AbstractDownSampler
-from hython.datasets import LSTMDataset
+from hython.datasets import LSTMDataset, get_dataset
 from hython.normalizer import Normalizer
-from hython.datasets.datasets import get_dataset
 
 
 class RNNDatasetGetterAndSplitter(DataSplitter):
