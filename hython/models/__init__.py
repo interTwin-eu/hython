92096fecd61b85418c5cf2b2d5a10a234194fce5
iacopoff <iacopo.ff@gmail.com>
2025-02-14 21:50:27 +0100

    fix

--- hython/models/__init__.py
+++ hython/models/__init__.py
@@ -1,5 +1,7 @@
 import torch
+from typing import Dict
 import importlib
+from pathlib import Path
 from .cudnnLSTM import CudaLSTM
 from .hybrid import Hybrid
 from .transferNN import TransferNN
@@ -10,41 +12,72 @@ def get_model_class(model_name: str):
     model = getattr(MODULE_MODELS, model_name)    
     return model
 
-def load_model(model_registry: dict, model_name):
-    if (mlflow_models := model_registry.get("mlflow")) is not None:
+def load_model(model_registry: dict, model_name, model = None):
+    model_config = model_registry.get(model_name)
+    model_uri = Path(model_config.get("model_uri"))
+    logger = model_config.get("logger")
+    
+    if logger == "mlflow":
         import mlflow
-        config = mlflow_models.get(model_name)
-        model_uri = config.get("model_uri")
-        model = mlflow.pytorch.load_model(model_uri)
-    elif (local_models := model_registry.get("local")) is not None:
-        config = local_models.get(model_name)
-        model = get_model_class(model_name)
-        model_fp = config.get("model_uri")
-        model.load_state_dict(torch.load(model_fp))
+        model = mlflow.pytorch.load_model(str(model_uri))
+
+    elif logger == "local":
+        from omegaconf import OmegaConf
+        ext = model_uri.suffix
+        if model is None:
+            raise ValueError("For loading a model from a local source, model must be an instantiated model class")
+        if ext == ".yaml" or ext == ".yml":
+            config = OmegaConf.load(model_uri)
+            
+            model_uri = config.model_logger[model_name]["model_uri"]
+            # model_uri = Path(config.work_dir) / f"{config.experiment_name}_{config.experiment_run}" / config.model_file_name
+        elif ext == ".pt" or  ext == "pth":
+            pass
+        model.load_state_dict(torch.load(model_uri))
     return model
 
+def log_model(model_registry: dict, model_name, model):
+    model_config = model_registry.get(model_name)
+    model_uri = Path(model_config.get("model_uri"))
+    
+    logger = model_config.get("logger")
+    
+    if logger == "mlflow":
+        raise NotImplementedError
+    if logger == "local":
+        torch.save(model.state_dict(), model_uri)
 
-from typing import Dict
+
+
 class ModelLogAPI():
-
     def __init__(self, cfg):
         self.cfg = cfg.model_logger
-
-        if (mlflow_logger := self.cfg.get("mlflow")) is not None:
-            
-            self.model_components = {mlflow_logger[model]["model_component"]:model for model in mlflow_logger } 
-            self.model_log_names = {mlflow_logger[model]["model_component"]:mlflow_logger[model]["model_name"]
-                                    for model in mlflow_logger if mlflow_logger[model]["log"] == True} 
-
-    def load_model(self, model_component):
-            model_class_name = self.model_components[model_component]
-            return load_model(self.cfg, model_class_name)
+        self.model_components = {config["model_component"]:model for model, config in self.cfg.items()}
+        self.model_log_names = {config["model_component"]:config["model_name"] for model, config in self.cfg.items() if config["log"] is True}
+        self.model_load_names = {config["model_component"]:config["model_name"] for model, config in self.cfg.items() if config["load"] is True}
+        self.model_loggers = {config["model_component"]:config["logger"] for model, config in self.cfg.items()}
+        
+    def load_model(self, model_component, model_instantiated = None):
+        model_class_name = self.model_components[model_component]
+        return load_model(self.cfg, model_class_name, model = model_instantiated)
+        
+    def log_model(self, model_component, model):
+        model_name = self.model_log_names.get(model_component)
+        log_model(self.cfg, model_name, model)
         
     def get_model_component_class(self, model_component):
-            return self.model_components[model_component]
+        return self.model_components[model_component]
 
     def get_model_log_name(self, model_component):
-            return self.model_log_names[model_component]
+        return self.model_log_names[model_component]
         
     def get_model_log_names(self) -> Dict:
-            return self.model_log_names
+        return self.model_log_names
+
+    def get_model_load_name(self, model_component) -> Dict:
+        return self.model_load_names[model_component]
+
+    def get_model_load_names(self) -> Dict:
+        return self.model_load_names
+    def get_model_logger(self, model_component):
+        return self.model_loggers[model_component]
