e200fe273d92bc154aa486c0562a22b89b92a6ba
iacopoff <iacopo.ff@gmail.com>
2025-04-30 09:35:26 +0200

    dev

--- hython/sampler/__init__.py
+++ hython/sampler/__init__.py
@@ -10,7 +10,108 @@ from torch.utils.data import (
     RandomSampler,
 )
 
+import random
+def generate_time_idx(time_idx, seq_len, cell_size):
+    """Generate temporal indices to subset the temporal dimension of the spacetime_index.
+       It is supposed to be used at runtime, to randomly subset the spacetime_index."""
+    o = []
+    for c in range(cell_size):
+        for i in time_idx:
+            o.append(i + c*seq_len)
+    return o
 
+class RandomTemporalDynamicDownsampler(RandomSampler):
+    """Every epoch generate a random subset of temporal indices.
+    The generated indices (idx) are used by the dataloader to sample the dataset getitem[idx] """
+    def __init__(self, data_source, temporal_subset_size, replacement=False):
+        super(RandomTemporalDynamicDownsampler, self).__init__(data_source)
+        self.data_source = data_source
+        self.temporal_subset_size = temporal_subset_size
+        self.replacement = replacement
+        self.spacetime_index = self.data_source.spacetime_index
+        self.cell_size  = len(self.data_source.cell_coords[self.data_source.cell_linear_index ])
+        self.seq_len = self.data_source.seq_len
+        
+        self.time_size = self.data_source.time_size
+        # the total samples
+        self.total_subset_size = self.temporal_subset_size*self.cell_size
+
+    def __iter__(self):
+        if self.replacement:
+            time_indices = np.random.randint(0, self.time_size - self.seq_len, self.temporal_subset_size)
+        else:
+            time_indices = random.sample(range(self.time_size - self.seq_len), self.temporal_subset_size)
+            
+        indeces = generate_time_idx(time_indices, self.seq_len, self.cell_size )
+
+        return iter(indeces)
+    
+    def __len__(self):
+        return self.total_subset_size
+    
+class SequentialTemporalDynamicDownsampler(RandomSampler):
+    """Every epoch generate a random subset of temporal indices.
+    The generated indices (idx) are used by the dataloader to sample the dataset getitem[idx] """
+    def __init__(self, data_source, temporal_subset_size, replacement=False):
+        super(SequentialTemporalDynamicDownsampler, self).__init__(data_source)
+        self.data_source = data_source
+        self.temporal_subset_size = temporal_subset_size
+        self.replacement = replacement
+        self.spacetime_index = self.data_source.spacetime_index
+        self.cell_size  = len(self.data_source.cell_coords[self.data_source.cell_linear_index ])
+        self.seq_len = self.data_source.seq_len
+        
+        self.time_size = self.data_source.time_size
+        # the total samples
+        self.total_subset_size = self.temporal_subset_size*self.cell_size
+        print(self.total_subset_size)
+
+    def __iter__(self):
+        if self.replacement:
+            time_indices = np.random.randint(0, self.time_size - self.seq_len, self.temporal_subset_size)
+        else:
+            time_indices = random.sample(range(self.time_size - self.seq_len), self.temporal_subset_size)
+            
+        indeces = generate_time_idx(time_indices, self.seq_len, self.cell_size )
+
+        return iter(indeces)
+
+    def __len__(self):
+        return self.total_subset_size
+    
+class DistributedTemporalDynamicDownsampler(DistributedSampler):
+    """Every epoch generate a random subset of temporal indices.
+    The generated indices (idx) are used by the dataloader to sample the dataset getitem[idx] """
+    def __init__(self, data_source, temporal_subset_size, shuffle = True, replacement=False, sampling_kwargs={}):
+        super(DistributedTemporalDynamicDownsampler, self).__init__(
+            dataset=data_source, 
+            shuffle= shuffle,
+            **sampling_kwargs)
+                   
+        self.data_source = data_source
+        self.temporal_subset_size = temporal_subset_size
+        self.replacement = replacement
+        self.spacetime_index = self.data_source.spacetime_index
+        self.cell_size  = len(self.data_source.cell_coords[self.data_source.cell_linear_index])
+        self.seq_len = self.data_source.seq_len
+        
+        self.time_size = self.data_source.time_size
+        # the total samples
+        self.total_subset_size = self.temporal_subset_size*self.cell_size
+
+    def __iter__(self):
+        if self.replacement:
+            time_indices = np.random.randint(0, self.time_size - self.seq_len, self.temporal_subset_size)
+        else:
+            time_indices = random.sample(range(self.time_size - self.seq_len), self.temporal_subset_size)
+            
+        ind = generate_time_idx(time_indices, self.seq_len, self.cell_size )
+
+        return iter(ind)
+    
+    def __len__(self):
+        return self.total_subset_size
+    
 class SubsetSequentialSampler:
     r"""Samples elements sequentially, always in the same order.
 
@@ -31,11 +132,15 @@ class SubsetSequentialSampler:
 class SamplerBuilder(TorchSampler):
     def __init__(
         self,
+        cfg,
         pytorch_dataset: Dataset,
         sampling: str = "random",
         sampling_kwargs: dict = {},
         processing: str = "single-gpu",
     ):
+        
+        self.cfg = cfg 
+
         self.dataset = pytorch_dataset
 
         self.sampling = sampling
@@ -50,6 +155,15 @@ class SamplerBuilder(TorchSampler):
                 return RandomSampler(self.dataset, **self.sampling_kwargs)
             elif self.sampling == "sequential":
                 return SequentialSampler(self.dataset, **self.sampling_kwargs)
+            elif self.sampling == "temporal-downsampling-random":
+                return RandomTemporalDynamicDownsampler(
+                    self.dataset, self.cfg.temporal_subset_size,  **self.sampling_kwargs
+                )
+            elif self.sampling == "temporal-downsampling-sequential":
+                return SequentialTemporalDynamicDownsampler(
+                    self.dataset, self.cfg.temporal_subset_size, **self.sampling_kwargs
+                )
+
         if self.processing == "multi-gpu":
             if self.sampling == "random":
                 return DistributedSampler(
@@ -58,6 +172,14 @@ class SamplerBuilder(TorchSampler):
             elif self.sampling == "sequential":
                 return DistributedSampler(
                     self.dataset, shuffle=False, **self.sampling_kwargs
+                )
+            elif self.sampling == "temporal-downsampling-random":
+                return DistributedTemporalDynamicDownsampler(
+                    self.dataset, self.cfg.temporal_subset_size, shuffle=True, **self.sampling_kwargs
+                )
+            elif self.sampling == "temporal-downsampling-sequential":
+                return DistributedTemporalDynamicDownsampler(
+                    self.dataset, self.cfg.temporal_subset_size, shuffle=False, **self.sampling_kwargs
                 )
 
 
